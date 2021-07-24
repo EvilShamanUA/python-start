8c02bd94378ee9b5b5a6db4b02a4603f655a7a01
Vladislav Sherstyuk <shesrtyukvlad@gmail.com>
2021-07-25 00:33:00 +0300

    list remove

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 spisok = ["apple", "banana", 5, 12, 44, 4.8, True, "Dota", "GtaV", False, 55, 6]
-spisok.remove(5)
-spisok.remove(12)
-spisok.remove(44)
-spisok.remove(4.8)
-spisok.remove(55)
-spisok.remove(6)
-print(spisok)
+for i in spisok:
+    print(i)
