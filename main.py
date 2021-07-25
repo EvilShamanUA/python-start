bda2ce87fcab248a970ab5777c42266fc1deff92
Vladislav Sherstyuk <shesrtyukvlad@gmail.com>
2021-07-25 18:42:14 +0300

    list remove

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World")
+#print("Hello World")
+a = 50
+b = 25
+#print(a+b)
+
+spisok = ["apple", "banana", 5, 12, 44, 4.8, True, "Dota", "GtaV", False, 55, 6]
+for i in spisok:
+    print(i)
