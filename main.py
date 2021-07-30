e1d9893121aa032a802795d7fffa7cae3d048ed1
Vladislav Sherstyuk <shesrtyukvlad@gmail.com>
2021-07-30 21:43:28 +0300

    check str

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-#print("Hello World")
-a = 50
-b = 25
-#print(a+b)
-
 spisok = ["apple", "banana", 5, 12, 44, 4.8, True, "Dota", "GtaV", False, 55, 6]
 for i in spisok:
-    print(i)
+    if isinstance(i, str):
+        print(i)
